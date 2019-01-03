56cf69c290c8c0488f0ec38a2f382202672e68bb
Falco Peijnenburg <fpeijnenburg@gmail.com>
2019-01-03 19:44:47 +0100

    Fixed building for ghcjs 8.4

--- default.nix
+++ default.nix
@@ -1,16 +1,7 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs ? import ./nixpkgs.nix }:
 let
-  inherit (pkgs.haskell.packages) ghcjsHEAD;
+  inherit (pkgs.haskell.packages) ghcjs;
 
-  miso-src = pkgs.fetchFromGitHub {
-    rev = "bb2be3264ff3c6aa3b18e471d7cf04296024059b";
-    sha256 = "07k1rlvl9g027fp2khl9kiwla4rcn9sv8v2dzm0rzf149aal93vn";
-    owner = "haskell-miso";
-    repo = "miso";
-  };
-
-  miso-ghcjs = ghcjsHEAD.callCabal2nix "miso" miso-src {};
-
-  drv = ghcjsHEAD.callPackage ./pkg.nix { miso = miso-ghcjs; };
+  drv = ghcjs.callCabal2nix "miso-components-example" ./. {};
 in
   if pkgs.lib.inNixShell then drv.env else drv
